b39497f9d13c674414b6bde0c60b556d276066e9
Nathan Zeager <nathanzeager@hotmail.com>
2022-08-27 09:48:31 -0400

    Added Tag model

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Snippet, Language
+from .models import User, Snippet, Language, Tag
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Snippet)
 admin.site.register(Language)
+admin.site.register(Tag)
--- snippets/models.py
+++ snippets/models.py
@@ -10,8 +10,9 @@ class User(BaseUser):
 
 
 class Snippet(models.Model):
+    title = models.CharField(max_length=255, default='')
+    description = models.CharField(max_length=512, default='', blank=True)
     code = models.TextField()
-    description = models.CharField(max_length=512, default='')
     language = models.ForeignKey(
         'Language', on_delete=models.CASCADE, related_name='snippets', blank=True, null=True)
     # related name should be the plural of the model that it's in. This is a O2M relationship. A snippet has one user. A user can have many snippets.
@@ -21,11 +22,22 @@ class Snippet(models.Model):
     user = models.ManyToManyField(
         'User', related_name='snippets')
     created_at = models.DateTimeField(auto_now_add=True)
+    tag = models.ManyToManyField('Tag', related_name='snippets', blank=True)
 
     def __str__(self):
-        return f'{self.description} in {self.language}'
+        return f'{self.title} in {self.language}'
 
 
 class Language(models.Model):
     name = models.CharField(max_length=255)
     version = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} version {self.version}'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f'{self.name}'
