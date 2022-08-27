ffe368bfe42febbe75a2520dd966bb6c3c9d2004
Nathan Zeager <nathanzeager@hotmail.com>
2022-08-27 09:15:15 -0400

    Updated model behavior, added Language model

--- snippets/models.py
+++ snippets/models.py
@@ -10,10 +10,22 @@ class User(BaseUser):
 
 
 class Snippet(models.Model):
-    title = models.CharField(max_length=200)
-    code = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
+    code = models.TextField()
+    description = models.CharField(max_length=512, default='')
+    language = models.ForeignKey(
+        'Language', on_delete=models.CASCADE, related_name='snippets', blank=True, null=True)
+    # related name should be the plural of the model that it's in. This is a O2M relationship. A snippet has one user. A user can have many snippets.
+    # user = models.ForeignKey(
+    #     User, on_delete=models.CASCADE, related_name='snippets')
+    # if I want to have more than one user associated with a snipped, I use a M2M field
+    user = models.ManyToManyField(
+        'User', related_name='snippets')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.title}'
+        return f'{self.description} in {self.language}'
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=255)
+    version = models.FloatField(blank=True, null=True)
